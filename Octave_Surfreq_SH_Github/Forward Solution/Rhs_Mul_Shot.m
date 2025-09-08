43856ce6b920d7c347ceb58564f5959e80d5963c
Uygar Ceyhan <125125761+uygarcey@users.noreply.github.com>
2025-09-08 09:23:08 +0300

    Update Rhs_Mul_Shot.m

--- Octave_Surfreq_SH_Github/Forward Solution/Rhs_Mul_Shot.m
+++ Octave_Surfreq_SH_Github/Forward Solution/Rhs_Mul_Shot.m
@@ -7,13 +7,14 @@ src_index = int.srcx + (int.srcz-1)*int.nx;
 %f0=int.fc;
 f=int.mf;
   % t0 = -2/(pi*f0);
-    %% Wavelets
+    % Wavelets
  %ricker2= 1i*(2*pi*f).*sqrt(pi/f0).*exp(-.5*(f/f0).^2 + 1i*(2*pi*f)*t0);
  ricker1= sqrt(4/(pi*int.fc^2))*((f/int.fc).^2).*exp(-(f/int.fc).^2).*exp(-1i*2*pi*f/int.fc);
  %amp = (2.0./sqrt(pi)).*(f.^2/int.fc.^3).*exp(-(f/int.fc).^2);
 Rhs(sub2ind([int.nx*int.nz,int.nsrc],src_index(1:int.nsrc),1:int.nsrc)) = ricker1./int.dx^2;
 
 end
+
 
 
 
