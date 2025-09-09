2667feadce20cb00b3627a277ae41ffdcf6554b8
Uygar <125125761+uygarcey@users.noreply.github.com>
2025-09-09 09:04:34 +0300

    Update Extract.m

--- Octave_Surfreq_SH_Github/Forward Solution/Extract.m
+++ Octave_Surfreq_SH_Github/Forward Solution/Extract.m
@@ -1,5 +1,5 @@
-%This function is used to extract wave fields in receiver points. It is a
-%sparse matrix which holds only receiver points.
+%This function is used to extract wave fields in receiver points. It is a sparse matrix which contains only receiver points.
+%Developed by Uygar Ceyhan.
 function [S] = Extract(int)
 S = sparse(double(int.nz*int.nx),double(int.nrcv));
 rcv_index = int.rcvx + (int.rcvz-1)*int.nx; %Take receiver indexes
@@ -7,5 +7,6 @@ rcv_index = int.rcvx + (int.rcvz-1)*int.nx; %Take receiver indexes
 % col=1:int.nrcv;%colums
 S(sub2ind([int.nx*int.nz,int.nrcv],rcv_index(1:int.nrcv),1:int.nrcv)) = 1; % It equals 1 in receiver points ;
 end
+
 
 
