a0ec20d4a568942bff07946508b0a96cd86c4979
Uygar <125125761+uygarcey@users.noreply.github.com>
2025-09-09 09:03:05 +0300

    Update Analytical.m

--- Octave_Surfreq_SH_Github/Analytical Solution/Analytical.m
+++ Octave_Surfreq_SH_Github/Analytical Solution/Analytical.m
@@ -1,4 +1,5 @@
 %This code returns Analytical Solution of SH wave
+%Developed by Uygar Ceyhan
 
 function [Ush_anly,R] = Analytical(int,vs,rho)
 
@@ -16,5 +17,6 @@ Green = const*H01;
 Ush_anly=Green*ricker;
 
 end
+
 
 
